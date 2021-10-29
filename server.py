400d6c1266c58205b682e3e51319419c1f11250e
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-28 18:47:43 -0700

    add rows that are not static

--- server.py
+++ server.py
@@ -9,12 +9,12 @@ app = Flask(__name__)                                   # Create a new instance 
 # **** Default App Route **********************************************
 @app.route('/')                                         # The "@" decorator associates this route with the function immediately following
 def index():
-
     users = [
-    {'first_name' : 'Michael', 'last_name' : 'Choi'},
-    {'first_name' : 'John', 'last_name' : 'Supsupin'},
-    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'},
+        {'first_name' : 'Dwayne', 'last_name' : 'Johnson'}
     ]
 
     return render_template('index.html', users=users)
